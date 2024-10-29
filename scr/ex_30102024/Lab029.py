f0829e9bb906fe80c9f2d7addcab43a0cfc411e0
Madhukar <madhukar19996@gmail.com>
2024-10-29 19:49:28 +0530

    All the  12 files are updated till now commit

---
+++ scr/ex_30102024/Lab029.py
@@ -0,0 +1,7 @@
+#Wap calculate area of triangle take input from user
+
+lenght=float(int(input("Enter lenght of triangle\n ")))
+breath=float(int(input("Enter breath of triangle\n ")))
+Height=float(int(input("Enter height of triangle\n ")))
+Area= float((lenght*breath*Height)/2)
+print("Area of triangle is ", Area)
