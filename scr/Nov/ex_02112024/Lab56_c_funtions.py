fc61058d9f002a0740288d4e60b48f9ab454b01d
Madhukar <madhukar19996@gmail.com>
2024-11-18 05:56:15 +0530

    18-11-2024 files are pushed

---
+++ scr/Nov/ex_02112024/Lab56_c_funtions.py
@@ -0,0 +1,11 @@
+def even_odd(num):
+    if num % 2 ==0:
+        print("Number is even :", num)
+    else:
+        print("Number is odd :", num )
+
+even_odd(7)
+even_odd(8)
+even_odd(5)
+even_odd(4)
+
