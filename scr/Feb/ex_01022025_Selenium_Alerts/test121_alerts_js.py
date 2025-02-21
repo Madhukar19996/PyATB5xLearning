088f43a414ef92d796a97d51497c3aa63ec8a707
Madhukar <madhukar19996@gmail.com>
2025-02-21 08:26:09 +0530

    21-02-2025 Adding new files

--- scr/Feb/ex_01022025_Selenium_Alerts/test121_alerts_js.py
+++ scr/Feb/ex_01022025_Selenium_Alerts/test121_alerts_js.py
@@ -9,65 +9,84 @@ from selenium.webdriver.support import expected_conditions as EC
 
 @allure.title("Alerts")
 @allure.description("Verify Alerts")
-def test_alerts_js_alert_normal():
-    driver = webdriver.Chrome()
+# def test_alerts_js_alert_normal():
+#     driver = webdriver.Chrome()
+#     driver.get("https://the-internet.herokuapp.com/javascript_alerts")
+#
+#     element_prompt = driver.find_element(By.XPATH, "//button[@onclick='jsAlert()']")
+#     element_prompt.click()
+#
+#     # WebDriverWait(driver=driver, timeout=3).until(EC.alert_is_present())
+#
+#     alert = driver.switch_to.alert
+#     alert.accept()
+#
+#     result_text = driver.find_element(By.ID, "result").text
+#     assert result_text == "You successfully clicked an alert"
+#
+#
+#     time.sleep(5)
+#
+#
+#
+#
+#
+# def test_alerts_confirm():
+#     driver = webdriver.Chrome()
+#     driver.get("https://the-internet.herokuapp.com/javascript_alerts")
+#
+#     element_confirm = driver.find_element(By.XPATH, "//button[@onclick='jsConfirm()']")
+#     element_confirm.click()
+#
+#     WebDriverWait(driver=driver, timeout=5).until(EC.alert_is_present())
+#
+#     alert = driver.switch_to.alert
+#     alert.dismiss()
+#
+#     result_text = driver.find_element(By.ID, "result").text
+#
+#     assert result_text == "You clicked: Cancel"
+#
+#     time.sleep(5)
+#
+#
+#
+#
+#
+# def test_alerts_prompt():
+#     driver = webdriver.Chrome()
+#     driver.get("https://the-internet.herokuapp.com/javascript_alerts")
+#
+#     element_prompt = driver.find_element(By.XPATH, "//button[@onclick='jsPrompt()']")
+#     element_prompt.click()
+#
+#     WebDriverWait(driver=driver, timeout=5).until(EC.alert_is_present())
+#
+#     alert = driver.switch_to.alert
+#     alert.send_keys("Madhukar Pandey")
+#     alert.accept()
+#
+#     result_text = driver.find_element(By.ID, "result").text
+#
+#     assert result_text == "You entered: Madhukar Pandey"
+#
+#     time.sleep(5)
+####################################################################################################################
+# from selenium import webdriver
+
+def test_alert_testcase():
+
+    driver=webdriver.Chrome()
     driver.get("https://the-internet.herokuapp.com/javascript_alerts")
 
-    element_prompt = driver.find_element(By.XPATH, "//button[@onclick='jsAlert()']")
-    element_prompt.click()
+    elm=driver.find_element(By.XPATH,"//button[normalize-space()='Click for JS Alert']").click()
 
-    WebDriverWait(driver=driver, timeout=3).until(EC.alert_is_present())
-
-    alert = driver.switch_to.alert
+    alert=driver.switch_to.alert
+    time.sleep(1)
     alert.accept()
-
-    result_text = driver.find_element(By.ID, "result").text
-    assert result_text == "You successfully clicked an alert"
-
-
-    time.sleep(5)
+    time.sleep(2)
 
 
 
 
 
-def test_alerts_confirm():
-    driver = webdriver.Chrome()
-    driver.get("https://the-internet.herokuapp.com/javascript_alerts")
-
-    element_confirm = driver.find_element(By.XPATH, "//button[@onclick='jsConfirm()']")
-    element_confirm.click()
-
-    WebDriverWait(driver=driver, timeout=5).until(EC.alert_is_present())
-
-    alert = driver.switch_to.alert
-    alert.dismiss()
-
-    result_text = driver.find_element(By.ID, "result").text
-
-    assert result_text == "You clicked: Cancel"
-
-    time.sleep(5)
-
-
-
-
-
-def test_alerts_prompt():
-    driver = webdriver.Chrome()
-    driver.get("https://the-internet.herokuapp.com/javascript_alerts")
-
-    element_prompt = driver.find_element(By.XPATH, "//button[@onclick='jsPrompt()']")
-    element_prompt.click()
-
-    WebDriverWait(driver=driver, timeout=5).until(EC.alert_is_present())
-
-    alert = driver.switch_to.alert
-    alert.send_keys("Madhukar Pandey")
-    alert.accept()
-
-    result_text = driver.find_element(By.ID, "result").text
-
-    assert result_text == "You entered: Madhukar Pandey"
-
-    time.sleep(5)
